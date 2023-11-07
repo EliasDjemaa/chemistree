996b462757f305505bf66100babaaad65ebde94c
EliasDjemaa <92042793+EliasDjemaa@users.noreply.github.com>
2023-11-07 03:08:22 +0000

    added flexy responsivness for login page divs deskt=row mobi=colm

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -8,17 +8,20 @@ export const Login = () => {
     <div>
       <Navigation />
       <div className='content-container'>
-        <div className='left-login-form'>
+        <div className='left-content'>
           <h1>UNDERSTAND AND OPTIMIZE YOUR HEALTH!</h1>
-          <ul>
-            <li>Discover whats affecting your health</li>
-            <li>Track your health, symptoms, illnesses, and medicines all in one place</li>
-            <li>Track your digestive issues, such as stomach pain and nausea.</li>
-          </ul>
+          <div>
+            <ul id='left-content-list'>
+              <li>Discover whats affecting your health</li>
+              <li>Track your health, symptoms, illnesses, and medicines all in one place</li>
+              <li>Track your digestive issues, such as stomach pain and nausea.</li>
+            </ul>
+          </div>
+          
           </div>
         
-        <div className='right-login-content'>
-
+        <div className='right-login-form'>
+          <h1>test content</h1>
         </div>
 
         
