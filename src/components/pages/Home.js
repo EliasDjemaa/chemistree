c57d6f1c11d37b51205917be0ffa13384c84cc02
EliasDjemaa <92042793+EliasDjemaa@users.noreply.github.com>
2023-11-08 22:15:07 +0000

    changed branding and worked on home page
    branding is great right now, i love it and it helps with accessability issues of previous version....really need to work on the structure and responsiveness on home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,38 +8,56 @@ export const Home = () => {
       <div className='routeOutlet'>
         <Navigation />
       </div>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
-      <h1>test content</h1>
 
+      <div className='main-container'> {/* serves as the body*/}
+        <div id='hero'>
+          <div id='hero-left'>
+            <h1 id='hero-header'>
+              Where care givers and patients come together. 
+            </h1>
+          </div>
+          <div id='hero-right'>
+            <p id='sub-head'>
+              Whether you're a care-giver or patient, receive medication reminders, 
+              summary of medical symptoms and current patient condition.
+            </p>
+          </div>
+        </div>
 
+        <div className='sub-content'>
+          
+          <div id='sub-left'>
+            <h3 className='sub-header'>
+              I’m a caregiver
+            </h3>
+            <p>
+              Keep track of patient health metrics and share with the rest of the 
+              care team.
+            </p>
+          </div>
+
+          <div id='sub-right'>
+          <h3 className='sub-header'>
+              I’m a caregiver
+            </h3>
+            <p>
+              Keep track of patient health metrics and share with the rest of the 
+              care team.
+            </p>
+          </div>
+                  <hr  style={{
+              color: '#FFFFFF',
+              width: '20px',
+          }}/>
+          <hr className='dividerL'/>
+
+          <h3 className='sub-header'>
+            Simple, smarter, more 
+            rewarding care.
+          </h3>
+
+        </div>
+      </div>
         {/* Add more content as needed */}
       </div>
 
